a099e6eddcc9277974fe1c0ba0a0bc0fa80cfba1
sivagnani <merugagnani@gmail.com>
2022-11-08 19:45:10 +0530

    08/11/2022

--- JS/AddressBook.js
+++ JS/AddressBook.js
@@ -25,30 +25,29 @@ function load(){
     }
 }
 function displaySummary(contact,i){
-    let div = document.createElement("div");
-    div.className = variable;
-    div.id=variable+i;
-    div.setAttribute('onclick','displayDetails(this.id)');
-    div.innerHTML="<h1 class='Name'>"+contact.name+"</h1><p class='Mail'>"+contact.email+"</p><p class='Mobile'>"+contact.mobile+"</p>"
-    document.getElementById("summary").appendChild(div);
+    let div = "<div class='eachContactSummary'></div>";
+    $("#summary").append(div);
+    $("#summary div").last().attr("id",variable+i);
+    $("#summary div").last().attr("onclick","displayDetails(this.id)");
+    $("#summary div").last().html("<h1 class='Name'>"+contact.name+"</h1><p class='Mail'>"+contact.email+"</p><p class='Mobile'>"+contact.mobile+"</p>");
 }
 function displayDetails(id){
-    document.getElementById(id).className=variable+" active";
-    document.getElementsByClassName('contactInfo')[0].style.display="block";
-    document.getElementById('addDetails').style.display="none";
+    $("#"+id).addClass("active");
+    $('.contactInfo').css({display:"block"});
+    $('#addDetails').css({display:"none"});
     var i = id[id.length-1];
     for(let j in list){
         if(j!=i && list[j].name!=null){
-            document.getElementById(variable+j).className=variable;
+            $("#"+variable+j).removeClass("active");
         }
     }
     counter=i;
-    document.getElementById('detailedName').innerHTML=list[i].name;
-    document.getElementById('detailedEmail').innerHTML=list[i].email;
-    document.getElementById('detailedMobile').innerHTML=list[i].mobile;
-    document.getElementById('detailedLandline').innerHTML=list[i].landline;
-    document.getElementById('detailedWebsite').innerHTML=list[i].website;
-    document.getElementById('detailedAddress').innerHTML=list[i].address;
+    $('#detailedName').text(list[i].name);
+    $('#detailedEmail').text(list[i].email);
+    $('#detailedMobile').text(list[i].mobile);
+    $('#detailedLandline').text(list[i].landline);
+    $('#detailedWebsite').text(list[i].website);
+    $('#detailedAddress').text(list[i].address);
 }
 function require(text){
     if(text==""){
@@ -59,176 +58,174 @@ function require(text){
     }
 }
 function deleteDetails(){
-    let delname=document.getElementById('detailedName').textContent;
+    let delname=$('#detailedName').text();
     if(confirm("Are you sure you want to delete "+delname+"'s details")==true){
         for(let i=0;i<list.length;i++){
             if(list[i].name==delname){
-                document.getElementById(variable+i).innerHTML=null;
-                document.getElementById(variable+i).remove();
-                document.getElementsByClassName('contactInfo')[0].style.display="none";
-                list[i].name=null;
+                $("#"+variable+i).remove();
+                $(".contactInfo").css({display:"none"});
             }
         }
     }
 }
 function openForm(){
-    document.getElementById('newName').value="";
-    document.getElementById('newEmail').value="";
-    document.getElementById('newMobile').value="";
-    document.getElementById('newLandline').value="";
-    document.getElementById('newWebsite').value="";
-    document.getElementById('newAddress').value="";
-    document.getElementById('addDetails').style.display="block";
-    document.getElementsByClassName('contactInfo')[0].style.display="none";
-    document.getElementById('createButton').value='Add';
-    document.getElementById('createButton').setAttribute('onclick','createContact()');
+    $('#newName').val("");
+    $('#newEmail').val("");
+    $('#newMobile').val("");
+    $('#newLandline').val("");
+    $('#newWebsite').val("");
+    $('#newAddress').val("");
+    $('#addDetails').css({display:"block"});
+    $('.contactInfo').css({display:"none"});
+    $('#createButton').val('Add');
+    $('#createButton').attr('onclick','createContact()');
     for(let j in list){
-        document.getElementById(variable+j).className=variable;
+        $("#"+variable+j).removeClass("active");
     }
 }
 function createContact(){
     if(validateForm()){
-    if(confirm("Are you sure you want to new contact details")){
-        var name = document.getElementById('newName').value;
-        var email = document.getElementById('newEmail').value;
-        var mobile = document.getElementById('newMobile').value;
-        var landline = document.getElementById('newLandline').value;
-        var website = document.getElementById('newWebsite').value;
-        var address = document.getElementById('newAddress').value;
-        contact = new Contact(name,email,mobile,landline,website,address);
-        list.push(contact);
-        displaySummary(contact,list.length-1);
-        document.getElementById("createForm").reset();
-        displayDetails(variable+(list.length-1));
+        if(confirm("Are you sure you want to new contact details")){
+            var name = $('#newName').val();
+            var email = $('#newEmail').val();
+            var mobile = $('#newMobile').val();
+            var landline = $('#newLandline').val();
+            var website = $('#newWebsite').val();
+            var address = $('#newAddress').val();
+            contact = new Contact(name,email,mobile,landline,website,address);
+            list.push(contact);
+            displaySummary(contact,list.length-1);
+            $("#createForm").trigger("reset");
+            displayDetails(variable+(list.length-1));
+        }
     }
 }
-}
 function editDetails(){
-    document.getElementById('addDetails').style.display="block";
-    document.getElementsByClassName('contactInfo')[0].style.display="none";
-    document.getElementById('createButton').value='Edit';
-    document.getElementById('createButton').setAttribute('onclick','editContact()');
-    document.getElementById('newName').value = document.getElementById('detailedName').textContent;
-    document.getElementById('newEmail').value = document.getElementById('detailedEmail').textContent;
-    document.getElementById('newMobile').value = document.getElementById('detailedMobile').textContent;
-    document.getElementById('newLandline').value = document.getElementById('detailedLandline').textContent;
-    document.getElementById('newWebsite').value = document.getElementById('detailedWebsite').textContent;
-    document.getElementById('newAddress').value = document.getElementById('detailedAddress').textContent;
+    $('#addDetails').css({display:"block"});
+    $('.contactInfo').css({display:"none"});
+    $('#createButton').val('Edit');
+    $('#createButton').attr('onclick','editContact()');
+    $('#newName').val($('#detailedName').text());
+    $('#newEmail').val($('#detailedEmail').text());
+    $('#newMobile').val($('#detailedMobile').text());
+    $('#newLandline').val($('#detailedLandline').text());
+    $('#newWebsite').val($('#detailedWebsite').text());
+    $('#newAddress').val($('#detailedAddress').text());
 }
 function editContact(){
     if(validateForm()){
-    if(confirm("Are you sure you want to edit "+list[counter].name+"'s details")){
-        list[counter].name = document.getElementById('newName').value;
-        list[counter].email = document.getElementById('newEmail').value;
-        list[counter].mobile = document.getElementById('newMobile').value;
-        list[counter].landline = document.getElementById('newLandline').value;
-        list[counter].website = document.getElementById('newWebsite').value;
-        list[counter].address = document.getElementById('newAddress').value;
-        document.getElementById(variable+counter).getElementsByClassName('Name')[0].innerHTML=list[counter].name;
-        document.getElementById(variable+counter).getElementsByClassName('Mail')[0].innerHTML=list[counter].email;
-        document.getElementById(variable+counter).getElementsByClassName('Mobile')[0].innerHTML=list[counter].mobile;
-        displayDetails(variable+counter)
-    }
+        if(confirm("Are you sure you want to edit "+list[counter].name+"'s details")){
+            list[counter].name = $('#newName').val();
+            list[counter].email = $('#newEmail').val();
+            list[counter].mobile = $('#newMobile').val();
+            list[counter].landline = $('#newLandline').val();
+            list[counter].website = $('#newWebsite').val();
+            list[counter].address = $('#newAddress').val();
+            $("#"+variable+counter).find(".Name").text(list[counter].name);
+            $("#"+variable+counter).find('.Mail').text(list[counter].email);
+            $("#"+variable+counter).find('.Mobile').text(list[counter].mobile);
+            displayDetails(variable+counter)
+        }
     }
 }
 function validateName(){
-    let name = document.getElementById('newName').value;
+    let name = $('#newName').val();
     const validname =/^[a-zA-Z\s]{4,256}$/;
     if(require(name)){
-        if(name.match(validname)){
-            document.getElementById('nameError').innerHTML="&nbsp;";
+        if(validname.test(name)){
+            $('#nameError').html("&nbsp;");
             return true;
         }
         else{
-            document.getElementById('nameError').innerHTML="Enter valid Name";
+            $('#nameError').html("Enter valid Name");
             return false;
         }
     }
     else{
-        document.getElementById('nameError').innerHTML="Name is required";
+        $('#nameError').html("Name is required");
         return false;
     }
 }
 function validateEmail(){
-    let email = document.getElementById("newEmail").value;
+    let email = $("#newEmail").val();
     const mail = /^[a-zA-Z0-9.$_*]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]{2,}$/;
     if(require(email)){
-        if(email.match(mail)){
-            document.getElementById('emailError').innerHTML="&nbsp;";
+        if(mail.test(email)){
+            $('#emailError').html("&nbsp;");
             return true;
         }
         else{
-            document.getElementById('emailError').innerHTML="Enter valid Email";
+            $('#emailError').html("Enter valid Email");
             return false;
         }
     }
     else{
-        document.getElementById("emailError").innerHTML = "Please enter Email";
+        $("#emailError").html("Please enter Email");
         return false;
     }
 }
 function validateMobile(){
-    let mobile = document.getElementById('newMobile').value;
+    let mobile = $('#newMobile').val();
     const valid =/^[\+]+[0-9]{2,3}[\s]?[0-9]{3}[\s]?[0-9]{5,7}$/;
     if(require(mobile)){
-        if(mobile.match(valid)){
-            document.getElementById('mobileError').innerHTML="&nbsp;";
+        if(valid.test(mobile)){
+            $('#mobileError').html("&nbsp;");
             return true;
         }
         else{
-            document.getElementById('mobileError').innerHTML="Enter valid Mobile Number";
+            $('#mobileError').html("Enter valid Mobile Number");
             return false;
         }
     }
     else{
-        document.getElementById('mobileError').innerHTML="Mobile Number is required";
+        $('#mobileError').html("Mobile Number is required");
         return false;
     }
 }
 function validateLandline(){
-    let landline = document.getElementById('newLandline').value;
+    let landline = $('#newLandline').val();
     const valid =/^[0][0-9]{2,3}[\s]?[0-9]{3,4}[\s]?[0-9]{4}$/;
     if(require(landline)){
-        if(landline.match(valid)){
-            document.getElementById('landlineError').innerHTML="&nbsp;";
+        if(valid.test(landline)){
+            $('#landlineError').html("&nbsp;");
             return true;
         }
         else{
-            document.getElementById('landlineError').innerHTML="Enter valid Telephone Number";
+            $('#landlineError').html("Enter valid Telephone Number");
             return false;
         }
     }
     else{
-        document.getElementById('landlineError').innerHTML="Telephone Number is required";
+        $('#landlineError').html("Telephone Number is required");
         return false;
     }
 }
 function validateWebsite(){
-    let website = document.getElementById('newWebsite').value;
-    const valid =/^([https|http]:)?\/?\/?(www.)+[a-zA-Z0-9#!:?+=&%!.\-\/]+\.([a-zA-Z]+){2,}$/;
+    let website = $('#newWebsite').val();
+    const valid =/^(http(s)?:\/\/)?((www.)?)+[a-zA-Z0-9#!:?+=&%!.\-\/]+\.[a-zA-Z\/]{2,}$/;
     if(require(website)){
-        if(website.match(valid)){
-            document.getElementById('websiteError').innerHTML="&nbsp;";
+        if(valid.test(website)){
+            $('#websiteError').html("&nbsp;");
             return true;
         }
         else{
-            document.getElementById('websiteError').innerHTML="Enter valid Website";
+            $('#websiteError').html("Enter valid Website");
             return false;
         }
     }
     else{
-        document.getElementById('websiteError').innerHTML="Website is required";
+        $('#websiteError').html("Website is required");
         return false;
     }
 }
 function validateAddress(){
-    let address = document.getElementById('newAddress').value;
+    let address = $('#newAddress').val();
     if(require(address)){
-        document.getElementById('addressError').innerHTML="&nbsp;";
+        $('#addressError').html("&nbsp;");
         return true;
     }
     else{
-        document.getElementById('addressError').innerHTML="Address is required";
+        $('#addressError').html("Address is required");
         return false;
     }
 }
